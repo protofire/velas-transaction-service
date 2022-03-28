4069a76da840e8f9b9f97e8a610a82f01751387b
Aliaksei Malyshau <aliaksei.malyshau@altoros.com>
2022-03-28 10:09:53 +0300

    feat: Added CodeBuild files

---
+++ .codebuild/docker-push.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Get reposotory URI from argument
+repository=$1
+
+# Push Docker images to the repository
+now=$(date +%Y-%m-%d--%H-%M-%S)
+for image in transaction_nginx transaction_web transaction_worker transaction_scheduler; do
+  docker tag $image:latest $repository-$image:latest
+  docker push $repository-$image:latest
+  docker tag $image:latest $repository-$image:$now
+  docker push $repository-$image:$now
+done
